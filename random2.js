128a1d5841e9143b8b4c759897fbce0ffa94aaca
s9237915 <46262940+s9237915@users.noreply.github.com>
2019-03-27 20:03:03 +0700

    Update random2.js

--- random2.js
+++ random2.js
@@ -23,7 +23,9 @@ var x=Math.floor((Math.random()*5)+ 1);
 var xxx=null;
 if(x=="1"){xxx="/2019/03/the-differences-between-accounting-and.html"}
 if(x=="2"){xxx="/2019/03/3-top-erp-vendors-you-should-try-for.html"}
-if(x=="3"){xxx="/2019/03/3-cheaper-but-powerful-open-source-erp.html"}a_to_ve[i].href="http://www.healthyfamily98.bid"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="3"){xxx="/2019/03/3-cheaper-but-powerful-open-source-erp.html"}
+if(x=="4"){xxx="/2019/03/microsoft-dynamics-ax-review.html"}
+if(x=="5"){xxx="/2019/03/the-most-popular-tools-on-market.html"}a_to_ve[i].href="http://www.healthyfamily98.bid"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
